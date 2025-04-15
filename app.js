a796003d7d4a602f6f0a083621ffc7ae0e111a21
Айтикеева Мээрим Бекболотовна <meerim.aitikeeva@gmail.com>
2025-04-15 22:47:07 +0600

    Added pug template engine

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const {routes: adminRoutes} = require("./routes/admin");
+const { routes: adminRoutes } = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 const app = express();
+app.set("view engine", "pug");
+app.set("views", "views");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -15,7 +17,7 @@ app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use("/", (req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  res.status(404).render('404');
 });
 
 app.listen(4000);
--- routes/shop.js
+++ routes/shop.js
@@ -3,13 +3,14 @@ const path = require("path");
 const express = require("express");
 
 const rootDir = require("../utils/path");
-const {products} = require("./admin");
+const { products } = require("./admin");
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  console.log(products);
-  res.sendFile(path.join(rootDir, "views", "shop.html"));
+  // console.log(products);
+  // res.sendFile(path.join(rootDir, "views", "shop.html"));
+  res.render("shop", { title: "Shop", prods: products });
 });
 
 module.exports = router;
