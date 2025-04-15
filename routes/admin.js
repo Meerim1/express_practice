86b096a10476e782f71cae6fa94c673b15ed327b
Айтикеева Мээрим Бекболотовна <meerim.aitikeeva@gmail.com>
2025-04-15 20:38:37 +0600

    Sharing Data Across Requests & Users

--- routes/admin.js
+++ routes/admin.js
@@ -2,19 +2,21 @@ const path = require("path");
 
 const express = require("express");
 
-const rootDir = require('../utils/path')
+const rootDir = require("../utils/path");
 
 const router = express.Router();
+const products = [];
 
 //   /admin/add-product => GET
 router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 
 //   /admin/add-product => POST
 router.post("/add-product", (req, res, next) => {
-  console.log("req.body", req.body);
+  products.push({ title: req.body.title });
   res.redirect("/");
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
--- routes/shop.js
+++ routes/shop.js
@@ -2,11 +2,13 @@ const path = require("path");
 
 const express = require("express");
 
-const rootDir = require('../utils/path')
+const rootDir = require("../utils/path");
+const {products} = require("./admin");
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
+  console.log(products);
   res.sendFile(path.join(rootDir, "views", "shop.html"));
 });
 
